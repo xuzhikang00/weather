f53febb8b36af9d7fdbf3bdcf2a2b88781981fb8
xuzhikang <531433607@qq.com>
2020-05-28 09:56:36 +0800

    add commander and colors

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello Typescript')
+import commander from "commander"
+import colors from "colors"
+commander
+    .version("0.1.0")
+    .option("-c, --city [name]", "Add city name")
+    .parse(process.argv)
+if (process.argv.slice(2).length===0) {
+    commander.outputHelp(colors.red);
+    process.exit();
+}
+console.log(commander.city)
